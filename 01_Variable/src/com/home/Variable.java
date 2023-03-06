3e01f09b017104ff691aca5f7a088bc050ed70a5
winjun91 <winjun91@gmail.com>
2023-03-06 23:05:45 +0900

    변수 시작
    변수

---
+++ 01_Variable/src/com/home/Variable.java
@@ -0,0 +1,22 @@
+package com.home;
+
+public class Variable {
+	public static void main(String args[]) {
+		System.out.println("Hello, world");
+		System.out.print("Hello");
+		System.out.println(", world");
+		expresstion();
+	}
+	public static void expresstion() {
+		System.out.println(10+2);
+		int x = 100;
+		double pi = 3.14;
+		char ch = 'a';
+		String str = "abc";
+		System.out.println(x);
+		System.out.println(pi);
+		System.out.println(ch);
+		System.out.println(str);
+	}
+
+}
