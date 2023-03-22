0247fe9ffe2ffc4467f0c427e6074555ca05b593
winjun91 <winjun91@gmail.com>
2023-03-22 21:23:10 +0900

    Math.round()로 반올림하기

---
+++ 02_Operator/src/com/home/G_Operator.java
@@ -0,0 +1,13 @@
+package com.home;
+
+public class G_Operator {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);
+
+	}
+
+
+}
