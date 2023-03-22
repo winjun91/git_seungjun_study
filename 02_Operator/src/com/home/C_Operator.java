a98b44ea662e985be4a5b492a5aab7f46f1f5aad
winjun91 <winjun91@gmail.com>
2023-03-22 20:35:23 +0900

    부호 연산자

--- 02_Operator/src/com/home/C_Operator.java
+++ 02_Operator/src/com/home/C_Operator.java
@@ -1,5 +1,13 @@
 package com.home;
 
 public class C_Operator {
-	
+	public void operator4() {
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
 }
--- 02_Operator/src/com/home/Run.java
+++ 02_Operator/src/com/home/Run.java
@@ -4,10 +4,12 @@ public class Run {
 
 	public static void main(String[] args) {
 		A_Operator a = new A_Operator();
-			a.operator1();
+			//a.operator1();
 		B_Operator b = new B_Operator();
-			b.operator2();
-			b.operator3();
+			//b.operator2();
+			//b.operator3();
+		C_Operator c = new C_Operator();
+			c.operator4();
 	}
 
 }
