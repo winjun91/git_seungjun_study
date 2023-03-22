4e684efaec6758dfd10646f21bca0038cd861434
winjun91 <winjun91@gmail.com>
2023-03-22 21:25:32 +0900

    나머지 연산자
    예제 3-13

--- 02_Operator/src/com/home/G_Operator.java
+++ 02_Operator/src/com/home/G_Operator.java
@@ -7,12 +7,19 @@ public class G_Operator {
 		double shortPi = Math.round(pi * 1000) / 1000.0;
 		System.out.println(shortPi);
 		operator10();
+		operator11();
 	}
 	public static void operator10() {
 		int x = 10;
 		int y = 8;
 		System.out.printf("%d을 %d로 나누면, %n", x,y);
 		System.out.printf("몫을 %d이고, 나이지는 %d입니다.%n", x / y, x % y);
+	}
+	public static void operator11() {
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+		
 	}
 	
 
