489bb4b819f80ad09380f1c37e8d3b852a388370
winjun91 <winjun91@gmail.com>
2023-03-22 21:23:43 +0900

    나머지 연산자

--- 02_Operator/src/com/home/G_Operator.java
+++ 02_Operator/src/com/home/G_Operator.java
@@ -6,8 +6,14 @@ public class G_Operator {
 		double pi = 3.141592;
 		double shortPi = Math.round(pi * 1000) / 1000.0;
 		System.out.println(shortPi);
-
+		operator10();
 	}
-
+	public static void operator10() {
+		int x = 10;
+		int y = 8;
+		System.out.printf("%d을 %d로 나누면, %n", x,y);
+		System.out.printf("몫을 %d이고, 나이지는 %d입니다.%n", x / y, x % y);
+	}
+	
 
 }
