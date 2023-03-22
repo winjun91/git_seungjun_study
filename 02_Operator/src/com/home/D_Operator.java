c2722e4e6c4c4075da5198cdfe52e529c8ff06e1
winjun91 <winjun91@gmail.com>
2023-03-22 20:40:57 +0900

    형변환 연산자

---
+++ 02_Operator/src/com/home/D_Operator.java
@@ -0,0 +1,13 @@
+package com.home;
+
+public class D_Operator {
+
+	public static void main(String[] args) {
+		double d = 85.4;
+		int score = (int) d;
+		System.out.println("score=" + score);
+		System.out.println("d=" + d);
+	}
+
+	public void 
+}
